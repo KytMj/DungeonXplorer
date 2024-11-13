e1c06ba29d828495bd42bc301c3e109ca5ef8848
Jonas Le Duff <jonas.leduff@etu.unicaen.fr>
2024-11-13 11:56:21 +0100

    updating from get to post

--- views/chapter_view.php
+++ views/chapter_view.php
@@ -19,7 +19,26 @@ $chapter = $chapterController->getChapter($_GET['chapter']);
 
     <h2>Choisissez votre chemin:</h2>
     <ul>
-        <?php foreach ($chapter->getChoices() as $choice): ?>
+        <?php foreach ($chapter->getChoices() as $choice):
+            
+//             You cant redirect page with post values in php.
+
+// If you want then you have to use javascript as most sites are doing (if you have seen paypal or other sites saying “wait for 5 seconds”.).
+
+// You can do it as below.
+
+// <form name=‘fr’ action=‘redirect(.)php’ method=‘POST’>
+// <include type=‘hidden’ name=‘var1’ value=‘val1’>
+// <include type=‘hidden’ name=‘var2’ value=‘val2’>
+// </form>
+// <script type=‘text/javascript’>
+// document.fr.submit();
+// </script>
+
+// This will post all variables to redirect(.)php.
+
+// I hope this will help you.
+?>
             <li>
                 <a href="chapter_view.php?chapter=<?php echo $choice['chapter']; ?>">
                     <?php echo $choice['text']; ?>
@@ -29,3 +48,5 @@ $chapter = $chapterController->getChapter($_GET['chapter']);
     </ul>
 </body>
 </html>
+
+
