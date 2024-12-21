0346e20dcf9ec6815570fb424f317c37357ea2d2
Cassandre <joret.maelys@gmail.com>
2024-12-21 18:48:25 +0100

    Retrieving the header

--- views/header.php
+++ views/header.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -13,7 +12,9 @@
                 <a href="home"><img src="./../image/Logo.png" class="img-fluid" style="width: 150px; height: 150px;" alt="Logo Dungeon Xplorer" /></a>
                 <div class="button">
                     <a href="signin"><button type="button" id="NewAccountButton">Créer un compte</button></a>
-                    <a href="connexion"><button type="button" id="LogInButton" onclick="">Se connecter</button></a>
+                    <a href="connexion"><button type="button" id="LogInButton" onclick="">Connexion</button></a>
+                    <a href="deconnexionAccount"><button type="button" id="LogInButton" onclick="">Déconnexion</button></a>
                 </div>
+                <a href="account"><img src="./../image/Account.png" class="img-fluid" style="width: 50px; height: 50px;" alt="Icône compte" /></a>
             </nav>
         </header>
