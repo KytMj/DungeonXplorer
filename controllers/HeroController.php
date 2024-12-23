de5e2dbff427604a58d0cb29661a332f9756ae72
Cassandre <cassandre.yontailleux@gmail.com>
2024-12-23 20:26:53 +0100

    Trying to debug HeroController

--- controllers/HeroController.php
+++ controllers/HeroController.php
@@ -1,7 +1,6 @@
 <?php
-
-include_once "./../core/pdo_agile.php";
-require_once "./../models/Hero.php";
+include_once "./core/pdo_agile.php";
+require_once "./models/Hero.php";
 
 class HeroController {
     private $hero;
@@ -14,16 +13,27 @@ class HeroController {
         require_once 'views/hero_view.php';
     }
 
-    public function __construct($id) {
-        require("./../core/Database.php");
-        $tab = [];
-
-        //potentiellement fausse car pas de tests possibles pour l'instant 
-        $stmt = $db->prepare("SELECT * FROM Hero join Stat using(hero_id) join Equipement using(hero_id) WHERE hero_id = :id");
-        
-        $stmt->execute([':id' => $id]);
-        $tab = $stmt->fetch(PDO::FETCH_ASSOC);
-        $this->hero = new Hero($tab['hero_name'], $tab['stat_pv'], $tab['stat_mana'], $tab['stat_strength'], $tab['stat_initiative'], $tab['ite_armor']);
+    public function __construct() {
+        require("./core/Database.php");
+        if(isset($_SESSION['login'])){
+            $tab = [];
+            LireDonneesPDO2($db, "select hero_id from Quest where user_id = ( select user_id from User where user_mail = '".$_SESSION['login']."')", $tab); // Pour récupérer le hero_id
+            $hero_id = $tab[0]['hero_id'];
+            unset($tab);
+            if($hero_id != null){
+                //potentiellement fausse car pas de tests possibles pour l'instant 
+                $stmt = $db->prepare("SELECT * FROM Hero join Stat using(hero_id) join Equipement using(hero_id) WHERE hero_id = (select hero_id from :hero_id)");
+            
+                $stmt->execute([':id' => $id]);
+                $tab = $stmt->fetch(PDO::FETCH_ASSOC);
+                $this->hero = new Hero($tab['hero_name'], $tab['stat_pv'], $tab['stat_mana'], $tab['stat_strength'], $tab['stat_initiative'], $tab['ite_armor']);
+            }else{
+                $erreur = "Vous n'avez pas d'aventure en cours.";
+                $_SESSION['erreur'] = $erreur;
+                require_once 'views/404.php';
+                exit();
+            }
+        }
     }
 
     public function show() {
